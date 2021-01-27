0f6d9be346a2ff1e1f7a209cec4142bb7bcfdab6
zerasul <zerasul@gmail.com>
2021-01-27 22:50:29 +0100

    añadiendo web

--- src/main/kotlin/net/hacklab/springbootkotlin/WebController.kt
+++ src/main/kotlin/net/hacklab/springbootkotlin/WebController.kt
@@ -3,14 +3,29 @@ package net.hacklab.springbootkotlin
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+
+data class Pokemon(var id: Int,var nombre:String, var tipo:String)
 
 @Controller
-class HtmlController {
+class WebController {
 
-    @RequestMapping("/blog")
+    @GetMapping("/blog")
     fun blog(model: Model):String{
-        model["title"]="Blog"
-        return "blog"
+        model["title"]="Pokemon"
+        val listaPokemons = listOf<Pokemon>(Pokemon(1,"Pikachu","Rayo"),
+            Pokemon(2,"Squirtle", "Agua")
+        )
+        model["pokemons"]=listaPokemons
+        return "pokelista"
     }
+
+    @GetMapping("/pokemon/{id}")
+    fun pokemon(@PathVariable id:Int, model: Model):String{
+        model["title"]="Detalle Pokemon"
+        model["pokemon"]=Pokemon(1,"Pikachu","Rayo")
+        return "pokemon"
+    }
+
 }
