083ea596afa210d8178fa9dd268f1dd3024efc09
zerasul <zerasul@gmail.com>
2021-01-27 20:13:29 +0100

    subiendo test con fallo

--- src/test/kotlin/net/hacklab/springbootkotlin/RestControllerTests.kt
+++ src/test/kotlin/net/hacklab/springbootkotlin/RestControllerTests.kt
@@ -6,17 +6,40 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
+import org.springframework.web.client.RestTemplate
+import java.util.*
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RestControllerTests(@Autowired val restTemplate: TestRestTemplate) {
 
     @Test
     fun `assert Rest Controller to Hello World`() {
-        println(">> Rest Controller with HelloWorld")
+        println(">> Rest Controller with HelloWorld -> 401")
         val entity = restTemplate.getForEntity<String>("/greeting")
-        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).contains("Hello World")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.UNAUTHORIZED)
+
     }
+
+    @Test
+    fun `assert Rest Controller authenticated to Hello World`(){
+        println(">> Rest Controller Authenticated With Hello World")
+
+        val entity=restTemplate.exchange("http://localhost:8080/greeting",
+            HttpMethod.GET,HttpEntity<String>(createHttpHeaders("admin","admin"))
+            , String.class )
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK))
+    }
+
+    fun createHttpHeaders(user:String, pass:String):HttpHeaders {
+        var headers=HttpHeaders()
+        headers.add("Authorization",
+            "Basic " + Base64.getEncoder().encodeToString("$user:$pass".toByteArray()))
+        return headers
+    }
+
 
 }
